dc70dc8f3efc2a9c847b870ddb0f3a3870d4f80b
sushichan044 <71284054+sushichan044@users.noreply.github.com>
2025-11-09 19:30:50 +0900

    no inverse

--- models/transformer/feed_forward.py
+++ models/transformer/feed_forward.py
@@ -1,23 +1,14 @@
-from enum import IntEnum
 import torch
-from models.basic.linear import Linear
 from models.transformer.activation import SwiGLU
 
 
-class FFNHiddenLayerScale(IntEnum):
-    PassThrough = 1
-    Small = 2
-    Base = 4
-    Large = 8
-
-
 class FeedForwardSwiGLU(torch.nn.Module):
-    def __init__(self, d_model: int, hidden_scale: FFNHiddenLayerScale = FFNHiddenLayerScale.PassThrough):
+    def __init__(
+        self,
+        d_model: int,
+    ):
         super().__init__()
-        d_ff = d_model * hidden_scale
-
-        self._swiglu = SwiGLU(d_model, d_ff)
-        self._inverse = Linear(d_ff, d_model)
+        self._swiglu = SwiGLU(d_model, d_model)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        return self._inverse(self._swiglu(x))
+        return self._swiglu(x)

db79a0c31b5f1dad84cebd4feac39abd405adc95
sushichan044 <71284054+sushichan044@users.noreply.github.com>
2025-11-09 18:38:16 +0900

    perf: use 1.0 by default

--- models/transformer/activation.py
+++ models/transformer/activation.py
@@ -8,9 +8,27 @@ def sigmoid(x: torch.Tensor) -> torch.Tensor:
 
 
 class Swish(torch.nn.Module):
-    def __init__(self):
+    def __init__(self, beta: float = 1.0, mark_beta_learnable: bool = False):
+        """
+        Implements Swish.
+
+        See: https://arxiv.org/abs/1710.05941
+
+        Parameters
+        ----------
+        beta : float, optional
+            Sharpness parameter, by default 1.0
+        mark_beta_learnable : bool, optional
+            Whether beta is learnable, by default False
+
+            Why default to False?
+            In the original paper, it was shown experimentally that even when beta is marked as a learnable parameter, it converges to a value close to 1.0, indicating that fixing it to 1.0 in practice causes no issues.
+        """
         super().__init__()
-        self.beta = torch.nn.Parameter(torch.tensor(1.0))
+        if mark_beta_learnable:
+            self.beta = torch.nn.Parameter(torch.tensor(beta))
+        else:
+            self.beta = torch.tensor(beta)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         return x * sigmoid(self.beta * x)

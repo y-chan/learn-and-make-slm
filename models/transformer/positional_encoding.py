cc2eb65af4cf7efee63bc25e7feea281d56070be
helkun <farflylonglight@gmail.com>
2025-11-09 19:20:30 +0900

    Imple PositionalEncoding

--- models/transformer/positional_encoding.py
+++ models/transformer/positional_encoding.py
@@ -1,10 +1,18 @@
 from torch import nn, Tensor
-
+import torch
 
 class PositionalEncoding(nn.Module):
     def __init__(self, d_model: int):
         super().__init__()
-        # TODO: 初期化
+        self.d_model = d_model
 
     def forward(self, x: Tensor) -> Tensor:
-        raise NotImplementedError
+        seq_len = x.size(1)
+        pos = torch.arange(seq_len, dtype=torch.float).unsqueeze(1)
+        i = torch.arange(self.d_model, dtype=torch.float).unsqueeze(0)
+        angle_rates = 1 / torch.pow(10000, (2 * (i // 2)) / self.d_model)
+        pe = pos * angle_rates
+        pe[:, 0::2] = torch.sin(pe[:, 0::2])
+        pe[:, 1::2] = torch.cos(pe[:, 1::2])
+        pe = pe.unsqueeze(0)
+        return x + pe

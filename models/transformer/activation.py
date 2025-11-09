3b7c3aeaf2b1479af3508d7b6fcbda1096a56810
sushichan044 <71284054+sushichan044@users.noreply.github.com>
2025-11-09 18:20:17 +0900

    Implement Swish

--- models/transformer/activation.py
+++ models/transformer/activation.py
@@ -1,9 +1,14 @@
-from torch import nn, Tensor
+import torch
 
 
-class Swish(nn.Module):
+def sigmoid(x: torch.Tensor) -> torch.Tensor:
+    return 1 / (1 + (-x).exp())
+
+
+class Swish(torch.nn.Module):
     def __init__(self):
         super().__init__()
+        self.beta = torch.nn.Parameter(torch.tensor(1.0))
 
-    def forward(self, x: Tensor) -> Tensor:
-        raise NotImplementedError
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return x * sigmoid(self.beta * x)

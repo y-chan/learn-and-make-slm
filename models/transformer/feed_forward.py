5d7a4e459b4afe9425546dc676167e7f6b18843e
sushichan044 <71284054+sushichan044@users.noreply.github.com>
2025-11-09 18:20:52 +0900

    feat: Implement FFN with SwiGLU

--- models/transformer/feed_forward.py
+++ models/transformer/feed_forward.py
@@ -1,9 +1,17 @@
 from torch import nn, Tensor
 
+from models.basic.linear import Linear
+from models.transformer.activation import SwiGLU
+
+
 class FeedForwardSwiGLU(nn.Module):
     def __init__(self, d_model: int):
         super().__init__()
-        # TODO: 初期化
+        d_hidden = d_model * 4
+
+        # 内部的に入力の 4 倍の次元で計算してもとの次元に戻す
+        self._swiglu = SwiGLU(d_model, d_hidden)
+        self._inverse = Linear(d_hidden, d_model)
 
     def forward(self, x: Tensor) -> Tensor:
-        raise NotImplementedError
+        return self._inverse(self._swiglu(x))

cdef9fb13e333a1d8af6949edc83f14c405c8c17
helkun <farflylonglight@gmail.com>
2025-11-09 15:49:08 +0900

    create one_hot function

--- models/basic/embedding.py
+++ models/basic/embedding.py
@@ -1,16 +1,21 @@
 from torch import nn, Tensor
+import torch
 
 class Embedding(nn.Module):
     def __init__(self, num_embeddings: int, embedding_dim: int):
         super().__init__()
         self.num_embeddings = num_embeddings
         self.embedding_dim = embedding_dim
-        self.one_hot = nn.functional.one_hot
         self.w = nn.Parameter(torch.randn(num_embeddings, embedding_dim))
 
     def forward(self, x: Tensor) -> Tensor:
-        one_hot = self.one_hot(x, num_classes=self.num_embeddings).float()
+        one_hot = self.one_hot(x)
         return one_hot @ self.w
+    
+    def one_hot(self, x: Tensor) -> Tensor:
+        num_classes = self.num_embeddings
+        one_hot = torch.eye(num_classes)[x]
+        return one_hot.float()
 
 
 if __name__ == "__main__":

76bff500136004c13c9bac9e03146f79f6affc73
sushichan044 <71284054+sushichan044@users.noreply.github.com>
2025-11-09 19:16:21 +0900

    fix: no up scale by default

--- models/transformer/feed_forward.py
+++ models/transformer/feed_forward.py
@@ -5,18 +5,19 @@ from models.transformer.activation import SwiGLU
 
 
 class FFNHiddenLayerScale(IntEnum):
-    small = 2
-    base = 4
-    large = 8
+    PassThrough = 1
+    Small = 2
+    Base = 4
+    Large = 8
 
 
 class FeedForwardSwiGLU(torch.nn.Module):
-    def __init__(self, d_model: int, hidden_scale: FFNHiddenLayerScale = FFNHiddenLayerScale.base):
+    def __init__(self, d_model: int, hidden_scale: FFNHiddenLayerScale = FFNHiddenLayerScale.PassThrough):
         super().__init__()
-        d_hidden = d_model * hidden_scale
+        d_ff = d_model * hidden_scale
 
-        self._swiglu = SwiGLU(d_model, d_hidden)
-        self._inverse = Linear(d_hidden, d_model)
+        self._swiglu = SwiGLU(d_model, d_ff)
+        self._inverse = Linear(d_ff, d_model)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         return self._inverse(self._swiglu(x))

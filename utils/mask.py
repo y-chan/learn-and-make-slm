7e8d3a943fd0ea47d976a8fa3c3ba4500eddb398
y-chan <y-chan@y-chan.dev>
2025-11-27 22:03:57 +0900

    Replace Copy code license

--- utils/mask.py
+++ utils/mask.py
@@ -1,6 +1,18 @@
 # Copied from https://github.com/espnet/espnet/blob/63a14f5867a9b2062c74b676393ae92f20e2a708/espnet/nets/pytorch_backend/nets_utils.py#L64-L270
-# Copyright 2022 ESPnet Contributors
-#  Apache 2.0  (http://www.apache.org/licenses/LICENSE-2.0)
+#
+# Copyright 2017 Johns Hopkins University (Shinji Watanabe)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 from typing import Union, List, Optional
 

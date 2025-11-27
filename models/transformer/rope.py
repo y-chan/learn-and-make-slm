52e2a49d7e1689da54965c9e38cea2d9a8ceb09c
sushichan044 <71284054+sushichan044@users.noreply.github.com>
2025-11-27 17:57:23 +0900

    fix: add runtime shape checking

--- models/transformer/rope.py
+++ models/transformer/rope.py
@@ -1,9 +1,11 @@
+from beartype import beartype
+from jaxtyping import jaxtyped, Float
 from torch import Tensor, nn
 import torch
 
 
-def rotate_half(x: Tensor) -> Tensor:
-    # x: [..., dim]
+@jaxtyped(typechecker=beartype)
+def rotate_half(x: Float[Tensor, "..."]) -> Float[Tensor, "..."]:
     x = x.reshape(*x.shape[:-1], -1, 2)  # [..., dim/2, 2]
     x1, x2 = x[..., 0], x[..., 1]
     rotated = torch.stack((-x2, x1), dim=-1)  # [..., dim/2, 2]
@@ -19,17 +21,18 @@ class RotaryPositionalEncoding(nn.Module):
         _thetas = self._setup_thetas(dim)
         self.register_buffer("thetas", _thetas)
 
-    def _setup_thetas(self, dim: int) -> Tensor:
+    @jaxtyped(typechecker=beartype)
+    def _setup_thetas(self, dim: int) -> Float[Tensor, "{dim}"]:  # noqa: F821
         base = 10000
-        vec = 1.0 / (base ** (torch.arange(0, dim, 2, dtype=torch.float) / dim))  # (dim/2,)
-        return torch.repeat_interleave(vec, repeats=2)  # (dim,)
+        vec = 1.0 / (base ** (torch.arange(0, dim, 2, dtype=torch.float) / dim))
+        return torch.repeat_interleave(vec, repeats=2)
 
-    def forward(self, x: Tensor) -> Tensor:
-        # assert x dim is same as self.dim
-        assert x.size(-1) == self.dim, f"x dim {x.size(-1)} must be same as rope dim {self.dim}"
-
-        pos = torch.arange(1, 1 + x.size(0), dtype=torch.float, device=x.device)  # (seq_len,)
-        rotate = torch.outer(pos, self.thetas)  # (seq_len, dim)
+    @jaxtyped(typechecker=beartype)
+    def forward(self, x: Float[Tensor, "seq {self.dim}"]) -> Float[Tensor, "seq {self.dim}"]:  # noqa: F821
+        start = 1
+        end = start + x.size(-2)
+        pos = torch.arange(start, end, dtype=torch.float, device=x.device)  # (seq,)
+        rotate = torch.outer(pos, self.thetas)  # (seq, dim)
 
         return x * torch.cos(rotate) + rotate_half(x) * torch.sin(rotate)
 

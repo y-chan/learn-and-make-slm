7a8fd3882f5e1a06204c08f2a291b282dcbd396c
y-chan <y-chan@y-chan.dev>
2025-12-02 19:43:23 +0900

    add gpt-2 like model

--- models/transformer/decoder_layer.py
+++ models/transformer/decoder_layer.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from torch import nn, Tensor
 from models.transformer.attention import GroupedQueryAttention
 from models.transformer.feed_forward import FeedForwardSwiGLU
@@ -5,7 +7,39 @@ from models.basic.layer_norm import LayerNorm
 from jaxtyping import Float, Int
 
 
-class DecoderLayer(nn.Module):
+class GPT2DecoderLayer(nn.Module):
+    """
+    概ねGPT-2のDecoder Layerを再現している
+    異なるのはFeed Forward BlockがGELUではなくSwishを使っている点
+    """
+    def __init__(self, d_model: int, n_heads: int):
+        super().__init__()
+        self.d_model = d_model
+
+        self.multi_head_attention = MultiHeadAttention(
+            d_model=d_model, n_heads=n_heads
+        )
+        self.layer_norm1 = LayerNorm([d_model])
+        self.feed_forward_block = FeedForwardSwish(d_model)
+        self.layer_norm2 = LayerNorm([d_model])
+
+    def forward(
+        self,
+        x: Float[Tensor, "B S D={self.d_model}"],
+        seq_lens: Optional[Int[Tensor, "B"]] = None
+    ) -> Float[Tensor, "B S D"]:
+        residual = x
+        x = self.layer_norm1(x)
+        x = residual + self.multi_head_attention(x, seq_lens)
+
+        residual = x
+        x = self.layer_norm2(x)
+        x = residual + self.feed_forward_block(x)
+
+        return x
+
+
+class GPTOSSDecoderLayer(nn.Module):
     def __init__(self, d_model: int, n_heads: int, n_groups: int):
         super().__init__()
         self.d_model = d_model

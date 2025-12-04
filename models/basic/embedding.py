315c05102fb63941698095e070cbd62e1ed08f28
y-chan <y-chan@y-chan.dev>
2025-12-05 07:26:06 +0900

    avoid out of memory

--- models/basic/embedding.py
+++ models/basic/embedding.py
@@ -7,9 +7,10 @@ class Embedding(nn.Module):
         super().__init__()
         self.num_embeddings = num_embeddings
         self.embedding_dim = embedding_dim
-        self.one_hot_vec = torch.eye(num_embeddings)
+        # self.one_hot_vec = torch.eye(num_embeddings)
         self.w = nn.Parameter(torch.randn(num_embeddings, embedding_dim))
 
     def forward(self, x: Tensor) -> Tensor:
-        one_hot = self.one_hot_vec[x]
-        return one_hot @ self.w
+        # one_hot = self.one_hot_vec[x]
+        # return one_hot @ self.w
+        return self.w[x]

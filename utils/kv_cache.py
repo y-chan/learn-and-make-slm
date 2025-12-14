9d273e170959be6380213a0e035ac7fe46fba626
sushichan044 <71284054+sushichan044@users.noreply.github.com>
2025-12-14 17:04:08 +0900

    hai

--- utils/kv_cache.py
+++ utils/kv_cache.py
@@ -1,15 +1,15 @@
 # This code is based on Hugging Face Transformers' cache utilities.
 # Original source:
 # https://github.com/huggingface/transformers/blob/40dc11cd3eb4126652aa41ef8272525affd4a636/src/transformers/cache_utils.py
-
+#
 # Copyright 2018- The Hugging Face team.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-
+#
 #    http://www.apache.org/licenses/LICENSE-2.0
-
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.

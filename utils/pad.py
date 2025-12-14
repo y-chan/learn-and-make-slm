b50c016d16010612b78e0ce4cf46fa63c1c4e9d1
Yuto Ashida <y-chan@y-chan.dev>
2025-12-14 17:03:17 +0900

    fix typing

--- utils/pad.py
+++ utils/pad.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def pad_1D(inputs: np.ndarray | list[np.ndarray], pad: float = 0) -> np.ndarray:
+def pad_1D(inputs: list[np.ndarray], pad: float = 0) -> np.ndarray:
     def pad_data(x: np.ndarray, length: int) -> np.ndarray:
         x_padded = np.pad(x, (0, length - x.shape[0]), mode="constant", constant_values=pad)
         return x_padded
